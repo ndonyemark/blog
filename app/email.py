d1c73f3df537f773347190bb3d65e4e798f840e0
Ndonye Mark <ndonyemark@gmail.com>
2020-05-14 07:42:17 +0300

    add email file

--- app/email.py
+++ app/email.py
@@ -4,8 +4,4 @@ from . import mail
 
 def mail_message(subject, body,html, to, **kwargs):
 
-    sender_email = "itsmisty41@gmail.com"
-    email = Message(subject, sender=sender_email, recipients=to)
-    email.body = body
-    email.html = html
-    mail.send(email)
+    
